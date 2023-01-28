5913ce77a1f343700146c7bb94543517526a4c8c
陈新予 <63854505+misaki10032@users.noreply.github.com>
2023-01-28 14:52:01 +0800

    Update addTextToFile.sh

--- addTextToFile.sh
+++ addTextToFile.sh
@@ -5,3 +5,8 @@ now_push_time=$(date "+%Y-%m-%d %H:%M:%S")
 newMD_log="【${now_push_time}】-【调用接口：/api/news/list/top10】- 微博热搜爬取成功，推送10条致QQ群【DONE】"
 echo -e $newMD_log
 sed -i "N;2a ${newMD_log} \n" ./tbpc.md
+
+# 下载后续脚本必须的python包
+pip install lxml
+pip install json
+pip install datetime
