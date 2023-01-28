564baff7a779e51f4072d9c4c85627d1fd0f7c16
陈新予 <63854505+misaki10032@users.noreply.github.com>
2023-01-28 14:12:28 +0800

    Update addTextToFile.sh

--- addTextToFile.sh
+++ addTextToFile.sh
@@ -4,4 +4,4 @@
 now_push_time=$(date "+%Y-%m-%d %H:%M:%S")
 newMD_log="【${now_push_time}】-【调用接口：/api/news/list/top10】- 微博热搜爬取成功，推送10条致QQ群【DONE】"
 echo -e $newMD_log
-sed -i "${newMD_log}\n -- cxy \n" ./tbpc.md
+sed -i "N;2a ${newMD_log} \n" ./tbpc.md
